de04a79be42393878b3f4dd0ebf3544a5e714519
Thayna <thaynaminuzzo@gmail.com>
2019-03-19 19:06:26 -0300

    added keywords, operators and some literals.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 extern int running;
 extern int lineNumber;
 int yylex();
+extern char* yytext;
 extern FILE *yyin;
 
 
@@ -28,10 +29,34 @@ int main (int argc, char **argv)
 	{
 		tok = yylex();
 		if(!running) break;
+		fprintf(stdout, "%s: \t ", yytext);
 		switch(tok)
 		{
-			case KW_IF: fprintf(stdout, "KW_IF"); break;
-			default: fprintf(stdout, "UNKNOWN"); break;
+			case KW_BYTE: fprintf(stdout, "KW_BYTE "); break;
+			case KW_INT: fprintf(stdout, "KW_INT "); break;
+			case KW_FLOAT: fprintf(stdout, "KW_FLOAT "); break;
+			case KW_IF: fprintf(stdout, "KW_IF "); break;
+			case KW_THEN: fprintf(stdout, "KW_THEN "); break;	
+			case KW_ELSE: fprintf(stdout, "KW_ELSE "); break;
+			case KW_LOOP: fprintf(stdout, "KW_LOOP "); break;
+			case KW_LEAP: fprintf(stdout, "KW_LEAP "); break;
+			case OPERATOR_OR: fprintf(stdout, "OPERATOR_OR "); break;	
+			case OPERATOR_AND: fprintf(stdout, "OPERATOR_AND "); break;
+			case OPERATOR_NOT: fprintf(stdout, "OPERATOR_NOT "); break;
+			case KW_READ: fprintf(stdout, "KW_READ "); break;
+			case KW_PRINT: fprintf(stdout, "KW_PRINT "); break;
+			case KW_RETURN: fprintf(stdout, "KW_RETURN "); break;
+			case OPERATOR_LE: fprintf(stdout, "OPERATOR_LE "); break;
+			case OPERATOR_GE: fprintf(stdout, "OPERATOR_GE "); break;
+			case OPERATOR_EQ: fprintf(stdout, "OPERATOR_EQ "); break;	
+			case OPERATOR_DIF: fprintf(stdout, "OPERATOR_DIF "); break;	
+			case TOKEN_ERROR: fprintf(stdout, "TOKEN_ERROR "); break;
+			case TK_IDENTIFIER: fprintf(stdout, "TK_IDENTIFIER "); break;
+			case LIT_INTEGER: fprintf(stdout, "LIT_INTEGER "); break;
+			case LIT_FLOAT: fprintf(stdout, "LIT_FLOAT "); break;
+			case LIT_CHAR: fprintf(stdout, "LIT_CHAR "); break;
+			case LIT_STRING: fprintf(stdout, "LIT_STRING "); break;
+			default: fprintf(stdout, "UNKNOWN "); break;
 		}
 	}
 }
--- tokens.h
+++ tokens.h
@@ -7,22 +7,24 @@
  * valor ascii de caracteres isolados.
  */
 
-#define KW_CHAR       256
+#define KW_BYTE       256
 #define KW_INT        257
 #define KW_FLOAT      259
 #define KW_IF         261
 #define KW_THEN       262
 #define KW_ELSE       263
-#define KW_WHILE      264
+#define KW_LOOP       264
+#define KW_LEAP       265
 #define KW_READ       266
 #define KW_RETURN     267
 #define KW_PRINT      268
 #define OPERATOR_LE   270
 #define OPERATOR_GE   271
 #define OPERATOR_EQ   272
-#define OPERATOR_OR   273
-#define OPERATOR_AND  274
-#define OPERATOR_NOT  275
+#define OPERATOR_DIF  273
+#define OPERATOR_OR   274
+#define OPERATOR_AND  275
+#define OPERATOR_NOT  276
 #define TK_IDENTIFIER 280
 #define LIT_INTEGER   281
 #define LIT_FLOAT     282
